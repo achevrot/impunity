cbf4b0c06599ace0226d78971d71eb341eb3c9e4
achevrot <antoine.chevrot@onera.fr>
2023-04-26 14:23:18 +0200

    revert for the test

--- tests/test_assign.py
+++ tests/test_assign.py
@@ -66,7 +66,8 @@ class Assign(unittest.TestCase):
 
     def test_assign_incompatible_unit(self) -> None:
         def test_assign_incompatible_unit() -> None:
-            pass
+            alt_m: "m" = 1000
+            alt_ft: "K" = alt_m
 
         with self.assertLogs("impunity.visitor", level="WARNING") as cm:
             impunity(test_assign_incompatible_unit)
