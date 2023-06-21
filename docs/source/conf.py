7b8ecf99cb8b6588e5d6f7c7574b8d0a96a63c96
achevrot <antoine.chevrot@onera.fr>
2023-06-21 10:52:54 +0200

    more docs and docstrings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ exclude_patterns = []  # type: ignore
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 
 # -- Extension configuration -------------------------------------------------
